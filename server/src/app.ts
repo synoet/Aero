a11663ca0ce7bed99be89872a105290c754c5f31
nysteo <teonys@nyu.edu>
2021-04-01 23:12:20 -0400

    remove

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,11 +18,6 @@ app.get("/groupmembers", (req: express.Request, res: express.Response) => {
     res.status(200).send(group);
 })
 
-app.get("/groupmembers?name=:name", (req: express.Request, res: express.Response) => {
-    let name = req.params.name;
-    res.status(200).send(name == 'teo' ? group[0] : name == 'soji' ? group[1]: "Not Found");
-})
-
 app.use(express.json());
 
 server.listen(port, () => {
