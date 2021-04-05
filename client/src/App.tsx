dddfc23be508379ca7b9eb88a48f693a5f3c8bb6
nysteo <teonys@nyu.edu>
2021-04-05 16:15:37 -0400

    Front End Started

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Project <Code fontSize="xl">Aero</Code> is coming soon.
+          </Text>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
