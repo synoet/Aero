9b12318f1736df89dc673a1d433d4515a7d1f526
nysteo <teonys@nyu.edu>
2021-04-08 14:12:14 -0400

    Flight View

--- client/src/Pages/Flights/index.tsx
+++ client/src/Pages/Flights/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {VStack, Wrap, WrapItem } from "@chakra-ui/react"
 import FlightList from './FlightList';
 import Trending from './Trending';
 import Navbar from '../../Components/Navbar'
 
 const Flights = () => {
+    const [delayedFlights, setDelayedFlights] = useState();
+    const [upcomingFlights, setUpcomingFlights] = useState();
+    const [allFlights, setAllFlights] = useState();
+
     return (
         <VStack>
             <Wrap spacing = {"3rem"} direction = "column">
