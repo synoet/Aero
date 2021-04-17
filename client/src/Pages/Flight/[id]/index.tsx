e3cea33049f1490834643774246ce5451ffcb095
nysteo <teonys@nyu.edu>
2021-04-17 17:54:21 -0400

    returning flights

--- client/src/Pages/Flight/[id]/index.tsx
+++ client/src/Pages/Flight/[id]/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Layout from '../../../components/Layout';
-import {chakra, Flex, HStack, VStack, Center} from '@chakra-ui/react';
+import {chakra, Flex, HStack, VStack, Center, Button, Divider} from '@chakra-ui/react';
 import styled from 'styled-components';
 import { FiArrowRight, FiArrowLeft } from 'react-icons/fi'
 import smallcircle from '../../../images/smallcircle.svg';
@@ -14,6 +14,7 @@ const Flight: React.FC<any> = ({match}: {match: any}) => {
     const {params: {flightId} } = match;
     const [isLoading, setIsLoading] = useState(true);
     const [flight, setFlight] = useState<IFlight>(mockFlight);
+    const [returns, setReturns] = useState([]);
 
     const screenType = useScreenType();
 
@@ -31,7 +32,19 @@ const Flight: React.FC<any> = ({match}: {match: any}) => {
             setFlight(res);
         }).catch((err) => console.log(err))
         .then(() => setIsLoading(false));
+
+        fetch(`https://projectaero-api.herokuapp.com/flights/${flightId}/returns`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            setReturns(res);
+        }).catch((err) => console.log(err))
     }, [])
+
 
     return (    
         <>
@@ -103,6 +116,69 @@ const Flight: React.FC<any> = ({match}: {match: any}) => {
                     </Flex>
                 </SelectedFlight>
 
+                <Divider marginTop='1rem'/>
+
+                <Flex w = '100%' marginTop= '1rem'><h1 style = {{fontWeight: '500', fontSize: '1.2rem'}}>Booking Options</h1></Flex>
+                <BookingOptions
+                    direction = 'column'
+                    w = '100%'
+                >
+                    <Option w = '100%' align = 'center' justify = 'space-between' direction = 'row' padding = '1rem'>
+                        <h1><span>Booking Agent:</span> Angelina Jolie</h1>
+                        <HStack>
+                            <h1 style = {{marginRight: '1rem'}}>{flight.base_price}$</h1>
+                            <Button align = 'center' bg="#6137FE"  color="white" onClick = {() => {history.push(`/flight/${flightId}`)}}>
+                                Buy
+                            </Button>
+                        </HStack>   
+                    </Option>
+                    <Option marginTop= '1rem' align = 'center' w = '100%' justify = 'space-between' direction = 'row' padding = '1rem'>
+                        <h1><span>Booking Agent:</span> Angelina Jolie</h1>
+                        <HStack align = 'center'>
+                            <h1 style = {{marginRight: '1rem'}}>{flight.base_price}$</h1>
+                            <Button bg="#6137FE"  color="white" onClick = {() => {history.push(`/flight/${flightId}`)}}>
+                                Buy
+                            </Button>
+                        </HStack>   
+                    </Option>
+                </BookingOptions>
+
+                <Divider marginTop='1rem'/>
+
+                <Flex w = '100%' marginTop= '1rem'><h1 style = {{fontWeight: '500', fontSize: '1.2rem'}}>Returning Flights</h1></Flex>
+                {returns &&
+                    <>
+                        <Center><p>{(returns.length === 0) ? 'No Flights': ''}</p></Center>
+                        <ReturningFlights
+                        
+                        direction = 'column'
+                        w = '100%'
+                        >
+                            {returns.map((flight: any) => {
+                                const departureDate = new Date(flight.departure_date)
+                                const arrivalDate = new Date(flight.arrival_date)
+                                return (
+                                    <ReturnFlight marginTop= '1rem' align = 'center' w = '100%' justify = 'space-between' direction = 'row' padding = '1rem'>
+                                        <HStack align = 'center'>
+                                            <p>{flight.departure_airport_name} </p>
+                                                <ArrowIcon size = {15}/>
+                                            <p>{flight.arrival_airport_name}</p>
+                                        </HStack>
+                                        <p>{`${arrivalDate.getHours() < 10 ? '0' : ''}${arrivalDate.getHours()}:${arrivalDate.getMinutes() < 10 ? '0': ''}${arrivalDate.getMinutes()}`}</p>
+                                        <p>{`${departureDate.getHours() < 10 ? '0' : ''}${departureDate.getHours()}:${departureDate.getMinutes() < 10 ? '0': ''}${departureDate.getMinutes()}`}</p>
+                                        <Button bg="#6137FE"  color="white" onClick = {() => {history.push(`/flight/${flightId}`)}}>
+                                            View Flight
+                                        </Button>
+
+                                    </ReturnFlight>
+
+                                )
+                            })}
+
+                        </ReturningFlights>
+                    </>
+                }
+
             </Layout>
         }
         </>
@@ -157,4 +233,33 @@ h1 {
     font-size: 1.2rem;
     font-weight: 500;
 }
+`;
+
+const BookingOptions = styled(Flex)`
+margin-top: 1rem;
+
+h1 {
+    font-size: 1.2rem;
+    font-weight: 500;
+}
+`;
+
+const ReturningFlights = styled(Flex)``;
+
+const Option = styled(Flex)`
+border: 1.5px solid #E9E9E9;
+box-sizing: border-box;
+border-radius: 10px;
+
+h1 {
+    span {
+        opacity: .6;
+    }
+}
+`
+const ReturnFlight = styled(Flex)`
+border: 1.5px solid #E9E9E9;
+box-sizing: border-box;
+border-radius: 10px;
+
 `;
