4d4dd1aed593b3757bb10ae0d49c5958b54b3bc4
nysteo <teonys@nyu.edu>
2021-04-20 14:35:27 -0400

    refactor Staff model

--- server/src/models/staff.model.ts
+++ server/src/models/staff.model.ts
@@ -1,10 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, {Schema, Document} from 'mongoose';
 import {MongooseService} from '../services/mongoose.service';
-const StaffSchema = new mongoose.Schema({
+
+const mongo = MongooseService.getInstance().getMongoose();
+
+export interface IStaff extends Document{
+    _id: string;
+    username: string;
+    airling_name: string;
+    password: string;
+
+}
+
+const StaffSchema: Schema = new mongoose.Schema({
+    _id: String,
     username: String,
     airline_name: String,
     password: String
 
 });
-const Staff = MongooseService.getInstance().getMongoose().model('Staff', StaffSchema);
+const Staff = mongo.model<IStaff>('Staff', StaffSchema);
+
 export default Staff;
